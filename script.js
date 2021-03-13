974c529c8daf3879e67e4a6bf823640c56b6a24f
NerisaK <nerisakalosin@seznam.cz>
2021-03-13 12:10:34 +0100

    added function to set date

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 // Weather API https://openweathermap.org/api 
+
+const today = document.querySelector(".today");
+const days = document.querySelectorAll(".day");
+const timeSpan = document.querySelector("#weekday");
+const dateSpan = document.querySelector("#date");
+
+// *** Setting date and weather *** //
+
+function whichDay(dayNum){
+    let text;
+    switch (dayNum) {
+    case 0: text = "Monday"; break;
+    case 1: text = "Tuesday"; break;
+    case 2: text = "Wednesday"; break;
+    case 3: text = "Thursday"; break;
+    case 4: text = "Friday"; break;
+    case 5: text = "Saturday"; break;
+    case 6: text = "Sunday"; break;
+    default: console.log("Invalid day number!");
+    }
+    return text;
+};
+
+function setDate() {
+    let date = new Date();
+    let dayWeek = date.getDay();    
+    let dayWeekName = whichDay(dayWeek);    
+    let timeStr = dayWeekName;
+    let dateStr = date.toLocaleDateString();
+    timeSpan.innerHTML = timeStr;
+    dateSpan.innerHTML = dateStr;
+};
+
+setDate();
+
+function setWeather() {
+    let temp = document.createElement("span");
+    temp.className = "dayTemp";
+    temp.innerText = "10°";
+    today.appendChild(temp);
+    for (day of days) {
+    let name = document.createElement("span");
+    name.className = "dayName";
+    name.innerText = "day";
+    let temp = document.createElement("span");
+    temp.className = "dayTemp";
+    temp.innerText = "10°";
+    day.appendChild(name);
+    day.appendChild(temp);
+    }
+};
+
+setWeather();
