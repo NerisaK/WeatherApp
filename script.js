d63c2495266a15ba5d260757fe8d64dd79ddcb7a
NerisaK <nerisakalosin@seznam.cz>
2021-03-13 13:32:49 +0100

    added weather icons

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// Weather API https://openweathermap.org/api 
+// Weather API https://openweathermap.org/api
+//******************
+// Weather icons:
+//******************
+// Create sunny and cloudy icons:
+// let img = document.createElement("div");
+// img.className = "weatherImg sunny/cloudy";
+// day.appendChild(img);
+//****************************************
+//Create partly_cloudy, rainy and thundery:
+// let img = document.createElement("div");
+// img.className = "weatherImg partly_cloudy/rainy/thundery";
+// let div1 = document.createElement("div");
+// div1.className = "partly_cloudy__sun/rainy__cloud/thundery__cloud"
+// let div2 = document.createElement("div");
+// div2.className = "partly_cloudy__cloud/rainy__rain/thundery__rain"
+// img.appendChild(div1);
+// img.appendChild(div2);
+// day.appendchild(img);
+
 
 const today = document.querySelector(".today");
 const days = document.querySelectorAll(".day");
@@ -11,13 +30,13 @@ let date;
 function whichDay(dayNum){
     let text;
     switch (dayNum) {
-    case 1: text = "Mon"; break;
-    case 2: text = "Tue"; break;
-    case 3: text = "Wed"; break;
-    case 4: text = "Thu"; break;
-    case 5: text = "Fri"; break;
-    case 6: text = "Sat"; break;
-    case 7: text = "Sun"; break;
+    case 1: text = "Monday"; break;
+    case 2: text = "Tuesday"; break;
+    case 3: text = "Wednesday"; break;
+    case 4: text = "Thursday"; break;
+    case 5: text = "Friday"; break;
+    case 6: text = "Saturday"; break;
+    case 7: text = "Sunday"; break;
     default: console.log("Invalid day number!");
     }
     return text;
@@ -41,12 +60,10 @@ setDate();
 // Sets the names of coming days according to what day is today
 function nextDays() {
     let i = date.getDay();
-    console.log(i);
     for (day of days) {
         if (i<7){i++;}
         else {i = 1;}        
-        console.log(i);
-        let dayWeekName = whichDay(i);
+        let dayWeekName = whichDay(i).slice(0, 3); //Shortened to 3 chars (e.g. Mon)
         let name = document.createElement("span");
         name.className = "dayName";
         name.innerText = dayWeekName;
@@ -60,15 +77,26 @@ nextDays();
 // Shows temperature - with a default value for now
 function setWeather() {
     // Set weather for today
+    let name = document.createElement("span");
+    name.innerHTML = "Today";
+    name.className = "dayName";
     let temp = document.createElement("span");
     temp.className = "dayTemp";
     temp.innerText = "10°";
+    let img = document.createElement("div");
+    img.className = "weatherImg cloudy";        
+    today.appendChild(img);
+    today.appendChild(name);
+    today.appendChild(document.createElement("br"));        
     today.appendChild(temp);
     // Set weather for next days
     for (day of days) {        
         let temp = document.createElement("span");
         temp.className = "dayTemp";
-        temp.innerText = "10°";        
+        temp.innerText = "10°";
+        let img = document.createElement("div");
+        img.className = "weatherImg sunny";
+        day.appendChild(img);        
         day.appendChild(temp);
     }
 };
