5f6999ddb47bb6506ebd461e503aa9e33557c860
NerisaK <nerisakalosin@seznam.cz>
2021-03-13 12:36:34 +0100

    added func to set names of upcoming days

--- script.js
+++ script.js
@@ -2,52 +2,74 @@
 
 const today = document.querySelector(".today");
 const days = document.querySelectorAll(".day");
-const timeSpan = document.querySelector("#weekday");
+const daySpan = document.querySelector("#weekday");
 const dateSpan = document.querySelector("#date");
+let date;
 
-// *** Setting date and weather *** //
 
+// Figures out which day of the week is now (or will be, depending on the value passed)
 function whichDay(dayNum){
     let text;
     switch (dayNum) {
-    case 0: text = "Monday"; break;
-    case 1: text = "Tuesday"; break;
-    case 2: text = "Wednesday"; break;
-    case 3: text = "Thursday"; break;
-    case 4: text = "Friday"; break;
-    case 5: text = "Saturday"; break;
-    case 6: text = "Sunday"; break;
+    case 1: text = "Mon"; break;
+    case 2: text = "Tue"; break;
+    case 3: text = "Wed"; break;
+    case 4: text = "Thu"; break;
+    case 5: text = "Fri"; break;
+    case 6: text = "Sat"; break;
+    case 7: text = "Sun"; break;
     default: console.log("Invalid day number!");
     }
     return text;
 };
 
+
+// Shows current date
 function setDate() {
-    let date = new Date();
+    date = new Date();
     let dayWeek = date.getDay();    
     let dayWeekName = whichDay(dayWeek);    
-    let timeStr = dayWeekName;
+    let dayStr = dayWeekName;
     let dateStr = date.toLocaleDateString();
-    timeSpan.innerHTML = timeStr;
+    daySpan.innerHTML = dayStr;
     dateSpan.innerHTML = dateStr;
 };
 
 setDate();
 
+
+// Sets the names of coming days according to what day is today
+function nextDays() {
+    let i = date.getDay();
+    console.log(i);
+    for (day of days) {
+        if (i<7){i++;}
+        else {i = 1;}        
+        console.log(i);
+        let dayWeekName = whichDay(i);
+        let name = document.createElement("span");
+        name.className = "dayName";
+        name.innerText = dayWeekName;
+        day.appendChild(name);  
+    }
+};
+
+nextDays();
+
+
+// Shows temperature - with a default value for now
 function setWeather() {
+    // Set weather for today
     let temp = document.createElement("span");
     temp.className = "dayTemp";
     temp.innerText = "10°";
     today.appendChild(temp);
-    for (day of days) {
-    let name = document.createElement("span");
-    name.className = "dayName";
-    name.innerText = "day";
-    let temp = document.createElement("span");
-    temp.className = "dayTemp";
-    temp.innerText = "10°";
-    day.appendChild(name);
-    day.appendChild(temp);
+    // Set weather for next days
+    for (day of days) {        
+        let temp = document.createElement("span");
+        temp.className = "dayTemp";
+        temp.innerText = "10°";        
+        day.appendChild(temp);
     }
 };
 
